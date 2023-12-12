a68a38121b3941597cffd4e4703f868ff3e0eca4
Kader2637 <kadergantengrek@gmail.com>
2023-12-12 16:51:03 +0700

    login pkl.hummatech

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,62 +1,187 @@
+import 'dart:ui';
+
+import 'package:pkl_apps/modules/login/botao_animado.dart';
+import 'package:pkl_apps/modules/login/input_customizado.dart';
 import 'package:flutter/material.dart';
-import 'package:pkl_apps/modules/home/home_screen.dart';
-import 'package:pkl_apps/services/auth/login_service.dart';
-import 'package:pkl_apps/widgets/message/errorMessage.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login-screen';
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  late LoginService login;
+class _LoginState extends State<LoginScreen> with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animacaoBlur;
+  Animation<double>? _animacaoFade;
+  Animation<double>? _animacaoSize;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    login = LoginService();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _animacaoBlur = Tween<double>(
+      begin: 50,
+      end: 0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.ease,
+      ),
+    );
+
+    _animacaoFade = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.easeInOutQuint,
+      ),
+    );
+
+    _animacaoSize = Tween<double>(
+      begin: 0,
+      end: 500,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.decelerate,
+      ),
+    );
+
+    _controller?.forward();
   }
 
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    // timeDilation = 8;
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(hintText: "Email"),
+      backgroundColor: Colors.white,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              AnimatedBuilder(
+                animation: _animacaoBlur!,
+                builder: (context, widget) {
+                  return Container(
+                    height: 400,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/fundo.png"),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: _animacaoBlur!.value,
+                        sigmaY: _animacaoBlur!.value,
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 10,
+                            child: FadeTransition(
+                              opacity: _animacaoFade!,
+                              child: Image.asset("images/detalhe1.png"),
+                            ),
+                          ),
+                          Positioned(
+                            left: 50,
+                            child: FadeTransition(
+                              opacity: _animacaoFade!,
+                              child: Image.asset("images/detalhe2.png"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
+                child: Column(
+                  children: [
+                    AnimatedBuilder(
+                      animation: _animacaoSize!,
+                      builder: (context, widget) {
+                        return Container(
+                          width: _animacaoSize?.value,
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 80,
+                                spreadRadius: 1,
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              const InputCustomizado(
+                                hint: 'e-mail',
+                                obscure: false,
+                                icon: Icon(Icons.person),
+                              ),
+                              Container(
+                                decoration: const BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey,
+                                      spreadRadius: 0.5,
+                                      blurRadius: 0.5,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const InputCustomizado(
+                                hint: 'Password',
+                                obscure: true,
+                                icon: Icon(Icons.lock),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    BotaoAnimado(controller: _controller!),
+                    const SizedBox(height: 10),
+                    FadeTransition(
+                      opacity: _animacaoFade!,
+                      child: const Text(
+                        "",
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 100, 127, 1),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(hintText: "Password"),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                login
-                    .doLogin(emailController.text, passwordController.text)
-                    .then((value) {
-                  print(value.status);
-                  print("NAME : ${value.email}");
-                  if (value.status == 200) {
-                    print("Berhasil login");
-                    Navigator.pushReplacementNamed(
-                        context, HomeScreen.routeName);
-                  } else {
-                    showErrorMessage("Username / password salah");
-                    print("Gagal login");
-                  }
-                });
-              },
-              child: Text("Login"))
-        ],
+        ),
       ),
     );
   }
